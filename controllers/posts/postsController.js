da402852a038b444c8612aca62d044a6e431c0a5
Abrham <abrsh6266@gmail.com>
2024-07-12 20:56:04 -0800

    minor updates

--- controllers/posts/postsController.js
+++ controllers/posts/postsController.js
@@ -59,3 +59,149 @@ exports.createPost = asyncHandler(async (req, res) => {
     post,
   });
 });
+exports.getPosts = asyncHandler(async (req, res) => {
+  // !find all users who have blocked the logged-in user
+  const loggedInUserId = req.userAuth?._id;
+  //get current time
+  const currentTime = new Date();
+  const usersBlockingLoggedInuser = await User.find({
+    blockedUsers: loggedInUserId,
+  });
+  // Extract the IDs of users who have blocked the logged-in user
+  const blockingUsersIds = usersBlockingLoggedInuser?.map((user) => user?._id);
+  //! Get the category, searchterm from request
+  const category = req.query.category;
+  const searchTerm = req.query.searchTerm;
+  //query
+  let query = {
+    author: { $nin: blockingUsersIds },
+    $or: [
+      {
+        shedduledPublished: { $lte: currentTime },
+        shedduledPublished: null,
+      },
+    ],
+  };
+  //! check if category/searchterm is specified, then add to the query
+  if (category) {
+    query.category = category;
+  }
+  if (searchTerm) {
+    query.title = { $regex: searchTerm, $options: "i" };
+  }
+  //Pagination parameters from request
+
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 5;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Post.countDocuments(query);
+
+  const posts = await Post.find(query)
+    .populate({
+      path: "author",
+      model: "User",
+      select: "email role username",
+    })
+    .populate("category")
+    .skip(startIndex)
+    .limit(limit)
+    .sort({ createdAt: -1 });
+  // Pagination result
+  const pagination = {};
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  res.status(201).json({
+    status: "success",
+    message: "Posts successfully fetched",
+    pagination,
+    posts,
+  });
+});
+
+//@desc  Get single post
+//@route GET /api/v1/posts/:id
+//@access PUBLIC
+exports.getPost = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id)
+    .populate("author")
+    .populate("category")
+    .populate({
+      path: "comments",
+      model: "Comment",
+      populate: {
+        path: "author",
+        select: "username",
+      },
+    });
+
+  res.status(201).json({
+    status: "success",
+    message: "Post successfully fetched",
+    post,
+  });
+});
+
+//@desc  Delete Post
+//@route DELETE /api/v1/posts/:id
+//@access Private
+
+exports.deletePost = asyncHandler(async (req, res) => {
+  //! Find the post
+  const postFound = await Post.findById(req.params.id);
+  const isAuthor =
+    req.user?._id?.toString() === postFound?.author?._id?.toString();
+  if (!isAuthor) {
+    throw new Error("Action denied, you are not the creator of this post");
+  }
+  await Post.findByIdAndDelete(req.params.id);
+  res.status(201).json({
+    status: "success",
+    message: "Post successfully deleted",
+  });
+});
+
+//@desc  update Post
+//@route PUT /api/v1/posts/:id
+//@access Private
+
+exports.updatePost = asyncHandler(async (req, res) => {
+  //!Check if the post exists
+  const { id } = req.params;
+  const postFound = await Post.findById(id);
+  if (!postFound) {
+    throw new Error("Post not found");
+  }
+  //! image update
+  const { title, category, content } = req.body;
+  const post = await Post.findByIdAndUpdate(
+    id,
+    {
+      image: req?.image ? req?.image : postFound?.image,
+      title: title ? title : postFound?.title,
+      category: category ? category : postFound?.category,
+      content: content ? content : postFound?.content,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(201).json({
+    status: "success",
+    message: "post successfully updated",
+    post,
+  });
+});
--- routes/posts/postsRoute.js
+++ routes/posts/postsRoute.js
@@ -1,16 +1,18 @@
 const express = require("express");
-const { createPost } = require("../../controllers/posts/postsController");
+const { createPost, getPosts, getPost, updatePost, deletePost } = require("../../controllers/posts/postsController");
 const isLoggedIn = require("../../middlewares/isLogged");
 
 const postRouter = express.Router();
 
 //create
 postRouter.post("/", isLoggedIn, createPost);
-// //?all
-// postRouter.get("/", getPosts);
-// // ! delete
-// postRouter.delete("/:id", isLoggedIn, deletePost);
-// // * Update
-// postRouter.put("/:id", isLoggedIn, updatePost);
+//? all
+postRouter.get("/", getPosts);
+//? single
+postRouter.get("/:id", getPost);
+//! delete
+postRouter.delete("/:id", isLoggedIn, deletePost);
+//* Update
+postRouter.put("/:id", isLoggedIn, updatePost);
 
 module.exports = postRouter;
